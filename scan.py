4451a88fe0e824439c32ec233ec92bdfb75034e9
Shepilov Vladislav <shepilov.v@progforce.com>
2013-12-19 15:54:33 +0400

    fixed many subdirrections under multiple run script
    Signed-off-by: Shepilov Vladislav <shepilov.v@progforce.com>

--- scan.py
+++ scan.py
@@ -8,10 +8,10 @@ from configparser import SafeConfigParser
 import logging
 import logging.config
 import os.path as _path
-import progressbar
-from progressbar import ETA, Bar
+from utils import progressbar
+from utils.progressbar import ETA, Bar
+import sys
 # import logging
-__log = []
 _log = logging
 _log.config.fileConfig(
     'settings/logging.conf',
@@ -19,10 +19,8 @@ _log.config.fileConfig(
 )
 
 home_dir = os.path.expanduser("~")
-init_path = "_music"
+init_path = "downloads_"
 path_to_scan = os.path.join(home_dir, init_path)
-end_path = os.path.join(home_dir, init_path + "_out")
-fsus.create_path(end_path)
 
 types = [
     'bittorrent',
@@ -41,16 +39,16 @@ types = [
 ]
 
 
-class GetListFiles:
+class Files_to_sort(object):
 
     def __init__(self, root_path):
-        # import pdb; pdb.set_trace
         self.config = SafeConfigParser()
         self.config.read('/home/kripton/Dropbox/scripts/settings/main.ini')
         # self.types = self.config["main"].items()[0][1].split(",")
         self.types = types
         self.root_path = root_path
-        self.log = []
+        self.l_filenames_parsed = None
+        self.filename_to_move = None
 
     def GetListParsed(self, FileList=None):
         #  list of files in input dirrectory
@@ -61,7 +59,7 @@ class GetListFiles:
         value = v
         bar = progressbar.ProgressBar(
             maxval=100.0,
-            widgets=['Getting filenames ', Bar('>'), ' ', ETA(), ]).start()
+            widgets=['Getting filenames      ', Bar('>'), ' ', ETA(), ]).start()
         Result = {}
         Result["other"] = []
 
@@ -71,7 +69,9 @@ class GetListFiles:
                     return _type
             return "other"
 
+        count = 0
         while len(FileList):
+            count += 1
             head_item = FileList[0]
             current_file = CMagic(head_item)
             type_item = get_type(current_file)
@@ -87,8 +87,10 @@ class GetListFiles:
                 value = 100.0
             bar.update(value)
         bar.finish()
+        sys.stdout.write("Find: %s files\n" % count)
 
         self.l_filenames_parsed = Result
+        _log.info("parsed list: %s" % self.l_filenames_parsed)
 
     def generate_filenames(self, lfp=None):
         if not lfp:
@@ -101,14 +103,14 @@ class GetListFiles:
             widgets=['Generate new filenames ', Bar('>'), ' ', ETA(), ]).start()
         result_list = {}
         count = 0
-        # import pdb; pdb.set_trace()
         for _type in lfp:
             current_list = lfp[_type]
-            # import pdb; pdb.set_trace()
             for init_file in current_list:
-                # import pdb; pdb.set_trace()
                 _dirname = _path.dirname(init_file)
-                final_dir = _path.join(_dirname, _type)
+                if _path.join(self.root_path, _type) == _dirname:
+                    final_dir = "not_move"
+                else:
+                    final_dir = _path.join(self.root_path, _type)
                 result_list[count] = {}
                 result_list[count]["init_file"] = init_file
                 result_list[count]["final_dir"] = final_dir
@@ -119,6 +121,8 @@ class GetListFiles:
                 bar.update(value)
         self.filename_to_move = result_list
         bar.finish()
+        sys.stdout.write("New filenames list generated!\n")
+        _log.info("New filenames list: %s" % self.filename_to_move)
 
     def move_files(self, list_files=None):
         if not list_files:
@@ -128,25 +132,35 @@ class GetListFiles:
         value = v
         bar = progressbar.ProgressBar(
             maxval=100.0,
-            widgets=['Moving files ', Bar('>'), ' ', ETA(), ]).start()
+            widgets=['Moving files           ', Bar('>'), ' ', ETA(), ]).start()
+        count = 0
         for i in list_files:
             current_item = list_files[i]
-            # import pdb; pdb.set_trace()
-            if _path.dirname(current_item["init_file"]) == current_item["final_dir"]:
+            if current_item["final_dir"] == "not_move":
                 continue
             else:
                 fsus.move_file(
                     current_item["init_file"], current_item["final_dir"])
+                count += 1
             value += v
             if value > 100.0:
                 value = 100.0
             bar.update(v)
         bar.finish()
+        sys.stdout.write("Moved %s files\n" % count)
+        _log.info("Moved %s files" % count)
 
-list_ = GetListFiles(path_to_scan)
-list_.GetListParsed()
-list_.generate_filenames()
-list_.move_files()
-
-(source, destination) = (end_path, path_to_scan)
-print "source: %s" % (path_to_scan)
+try:
+    _log.info("Start!")
+    list_ = Files_to_sort(path_to_scan)
+    _log.info("Getting parsed list of files.")
+    list_.GetListParsed()
+    _log.info("Generated new filenames.")
+    list_.generate_filenames()
+    _log.info("Moving files.")
+    list_.move_files()
+    sys.stdout.write("Files in '%s' sorted by type!\n" % (path_to_scan))
+    _log.info("Finish!")
+except KeyboardInterrupt:
+    sys.stdout.write("Stopped!\n")
+    _log.exception("Program aborted by user!")
